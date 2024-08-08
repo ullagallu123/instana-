dae76aee916c80c923071f82cba42361ed2577d2
sivaram <sivaram0434@gmail.com>
2024-08-08 20:36:15 +0530

    modification on code

--- shipping/src/main/java/com/instana/robotshop/shipping/JpaConfig.java
+++ shipping/src/main/java/com/instana/robotshop/shipping/JpaConfig.java
@@ -6,24 +6,35 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class JpaConfig {
     private static final Logger logger = LoggerFactory.getLogger(JpaConfig.class);
 
+    @Value("${DB_HOST}")
+    private String dbHost;
+
+    @Value("${DB_PORT}")
+    private String dbPort;
+
+    @Value("${DB_USER}")
+    private String dbUser;
+
+    @Value("${DB_PASSWD}")
+    private String dbPassword;
+
     @Bean
     public DataSource getDataSource() {
-        String JDBC_URL = String.format("jdbc:mysql://%s/cities?useSSL=false&autoReconnect=true", System.getenv("DB_HOST") == null ? "mysql" : System.getenv("DB_HOST"));
+        String JDBC_URL = String.format("jdbc:mysql://%s:%s/cities?useSSL=false&autoReconnect=true", dbHost, dbPort);
 
         logger.info("jdbc url {}", JDBC_URL);
 
-        DataSourceBuilder bob = DataSourceBuilder.create();
-
-        bob.driverClassName("com.mysql.jdbc.Driver");
-        bob.url(JDBC_URL);
-        bob.username("shipping");
-        bob.password("RoboShop@1");
-
-        return bob.build();
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.jdbc.Driver")
+                .url(JDBC_URL)
+                .username(dbUser)
+                .password(dbPassword)
+                .build();
     }
 }
