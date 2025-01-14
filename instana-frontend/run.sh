f7534da61200efc4f99295bc8301f2cc1db87c00
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-14 21:53:25 +0530

    add the

---
+++ instana-frontend/run.sh
@@ -0,0 +1,53 @@
+if [ -z "${CATALOGUE_HOST}" ]; then
+  echo Input CATALOGUE_HOST is missing
+  exit 1
+fi
+
+if [ -z "${CATALOGUE_PORT}" ]; then
+  echo Input CATALOGUE_PORT is missing
+  exit 1
+fi
+
+if [ -z "${USER_HOST}" ]; then
+  echo Input USER_HOST is missing
+  exit 1
+fi
+
+if [ -z "${USER_PORT}" ]; then
+  echo Input USER_PORT is missing
+  exit 1
+fi
+
+if [ -z "${CART_HOST}" ]; then
+  echo Input CART_HOST is missing
+  exit 1
+fi
+
+if [ -z "${CART_PORT}" ]; then
+  echo Input CART_PORT is missing
+  exit 1
+fi
+
+if [ -z "${SHIPPING_HOST}" ]; then
+  echo Input SHIPPING_HOST is missing
+  exit 1
+fi
+
+if [ -z "${SHIPPING_PORT}" ]; then
+  echo Input SHIPPING_PORT is missing
+  exit 1
+fi
+
+if [ -z "${PAYMENT_HOST}" ]; then
+  echo Input PAYMENT_HOST is missing
+  exit 1
+fi
+
+if [ -z "${PAYMENT_PORT}" ]; then
+  echo Input PAYMENT_PORT is missing
+  exit 1
+fi
+
+cat /opt/nginx.conf | envsubst >/etc/nginx/nginx.conf
+
+nginx -g 'daemon off;'
