c71108ebc0580ffad7ea77d842cce36c6d99639d
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-29 20:57:21 +0530

    Update rabbit.sh

--- rabbit/rabbit.sh
+++ rabbit/rabbit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e  # Exit on error
+
 rabbitmq-server -detached
 sleep 20
+
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
 rabbitmqctl stop
 exec rabbitmq-server
+
